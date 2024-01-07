d2d86ca0903387afc73b218d0ce8501f9a43d89a
Mercad's <mersad.shahidi@pardis.sharif.edu>
2024-01-07 13:57:29 +0330

    Add files via upload

---
+++ cmus/lib.pl
@@ -0,0 +1,10 @@
+/home/mercad/Music/1703425562_Ben - Fibonacci.mp3
+/home/mercad/Music/1703425550_Ben - Qasedak.mp3
+/home/mercad/Music/1703425526_Ben - Khon, Khak, Khashm, Khiregi.mp3
+/home/mercad/Music/1703425513_Ben - Adamaye Kohne, To Shahre No.mp3
+/home/mercad/Music/1703425497_Ben - Shogh.mp3
+/home/mercad/Music/1703425480_Ben - Rahaa.mp3
+/home/mercad/Music/1703425463_Ben - Man Kosh.mp3
+/home/mercad/Music/1703425450_Ben - Pishgoftar.mp3
+/home/mercad/Music/1703425433_Ben - Dey x Cee.mp3
+/home/mercad/Music/1703425414_Ben - Khaabi.mp3
